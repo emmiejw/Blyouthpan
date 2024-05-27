0889e8701273d255c3e69c32042ff68a1e6b7732
Emily Wallace - Price <60661797+emilywallace-price@users.noreply.github.com>
2024-05-27 15:53:03 +0100

    belts and braces

--- resources/views/partials/content.blade.php
+++ resources/views/partials/content.blade.php
@@ -10,23 +10,23 @@
 <section class="bg-white border-b py-8">
     <div class="container max-w-5xl mx-auto m-8">
         <h2 class="w-full p-4 my-2 text-5xl sm:text-2xl font-bold leading-tight text-center gradient-text">
-            {{ $content->title }}
+            {{ $content->title ?? null }}
         </h2>
         <div class="w-full mb-4">
             <div class="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
         </div>
         <h6 class="w-full my-2 p-4 sm:text-1xl text-2xl font-bold leading-tight text-center text-gray-800">
-            {{ $content->sub_title }}
+            {{ $content->sub_title ?? null }}
         </h6>
         <div class="flex flex-wrap">
             <div class="w-5/6 sm:w-1/2 p-4">
 
                 <p class="text-gray-600 mb-4">
-                    {{$content->section_one}}
+                    {{$content->section_one ?? null}}
                 </p>
             </div>
             <div class="w-full sm:w-1/2 p-4">
-                <img src="{{ $content->image_one }}" class="rounded-lg">
+                <img src="{{ $content->image_one ?? null }}" class="rounded-lg">
             </div>
         </div>
         <div class="w-full mb-4">
@@ -34,12 +34,12 @@
         </div>
         <div class="flex flex-wrap flex-col-reverse sm:flex-row">
             <div class="w-full sm:w-1/2 p-4 mt-4">
-                <img src="{{ $content->image_two }}" class="rounded-lg">
+                <img src="{{ $content->image_two ?? null }}" class="rounded-lg">
             </div>
             <div class="w-full sm:w-1/2 p-4 mt-4">
                 <div class="align-middle">
                     <p class="text-gray-600 mb-4">
-                        {{ $content->section_two }}
+                        {{ $content->section_two ?? null }}
                     </p>
                 </div>
             </div>
--- resources/views/partials/managers.blade.php
+++ resources/views/partials/managers.blade.php
@@ -12,16 +12,16 @@ use App\Models\Manager;
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8 xl:mt-16">
             @foreach(Manager::all() as $manager)
                 <div class="flex flex-col items-center p-8 transition-colors duration-200 transform cursor-pointer group hover:bg-blue-600 rounded-xl">
-                    <img class="object-cover w-32 h-32 rounded-full ring-4 ring-gray-300" src="{{ $manager->photo }}">
+                    <img class="object-cover w-32 h-32 rounded-full ring-4 ring-gray-300" src="{{ $manager->photo ?? null }}">
 
-                    <h1 class="mt-4 text-2xl font-semibold text-gray-700 capitalize group-hover:text-white">{{ $manager->name }}</h1>
+                    <h1 class="mt-4 text-2xl font-semibold text-gray-700 capitalize group-hover:text-white">{{ $manager->name ?? null }}</h1>
 
-                    <p class="mt-2 text-gray-500 capitalize dark:text-gray-300 group-hover:text-gray-300">{{ $manager->role }}</p>
+                    <p class="mt-2 text-gray-500 capitalize dark:text-gray-300 group-hover:text-gray-300">{{ $manager->role ?? null }}</p>
 
-                    <p class="mt-2 text-gray-500 capitalize dark:text-gray-300 group-hover:text-gray-300">{{ $manager->about_me }}</p>
+                    <p class="mt-2 text-gray-500 capitalize dark:text-gray-300 group-hover:text-gray-300">{{ $manager->about_me ?? null }}</p>
 
                     <div class="flex items-center justify-start">
-                        <a href="mailto:{{$manager->email}}" class="mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                        <a href="mailto:{{$manager->email ?? null}}" class="mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
                             Email me
                         </a>
                     </div>
