53cb7934b2e92b88a4fff98ef01fefb0fb16d69d
Emily Wallace - Price <60661797+emilywallace-price@users.noreply.github.com>
2024-05-31 20:53:38 +0100

    nav font size

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,8 +1,8 @@
 <nav id="header" class="fixed w-full z-30 top-0 text-white">
     <div class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
         <div class="pl-4 flex items-center">
-            <a class="gold-font no-underline hover:no-underline font-bold text-2xl md:text-3x" href="#">
-                Backfield & Langley Youth & Pan
+            <a class="gold-font no-underline hover:no-underline font-bold sm:text-sm md:text-3xl" href="#">
+                Blackfield & Langley Youth & Pan
             </a>
         </div>
     </div>
